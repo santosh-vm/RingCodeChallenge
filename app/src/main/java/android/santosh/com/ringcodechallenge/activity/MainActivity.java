6bda9777970b1050ec2ad6e419e840bb9f674454
Santosh V. Madhyastha <santosh@gogii.net>
2017-08-24 23:18:19 -0700

    First step of showing reddit post title in recycler view

--- app/src/main/java/android/santosh/com/ringcodechallenge/activity/MainActivity.java
+++ app/src/main/java/android/santosh/com/ringcodechallenge/activity/MainActivity.java
@@ -3,13 +3,18 @@ package android.santosh.com.ringcodechallenge.activity;
 import android.santosh.com.ringcodechallenge.R;
 import android.os.Bundle;
 import android.santosh.com.ringcodechallenge.adapter.RecyclerViewAdapter;
+import android.santosh.com.ringcodechallenge.listeners.MainControllerListener;
+import android.santosh.com.ringcodechallenge.model.RedditPost;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class MainActivity extends BaseActivity {
+import java.util.List;
+
+public class MainActivity extends BaseActivity implements MainControllerListener {
     private static String TAG = MainActivity.class.getSimpleName();
 
     private ProgressBar progressBar;
@@ -21,8 +26,8 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        appAPI.getMainController().addMainControllerListener(this);
         bindUIElements();
-        appAPI.getMainController().fetchPost();
     }
 
     private void bindUIElements() {
@@ -35,11 +40,49 @@ public class MainActivity extends BaseActivity {
 
         //RecyclerView
         recyclerViewAdapter = new RecyclerViewAdapter(this);
-        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         LinearLayoutManager recyclerViewLinearLayoutManger = new LinearLayoutManager(this);
         recyclerViewLinearLayoutManger.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(recyclerViewLinearLayoutManger);
         recyclerView.setAdapter(recyclerViewAdapter);
         recyclerView.setVisibility(View.GONE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (recyclerViewAdapter.getItemCount() <= 0) {
+            Log.d(TAG,"recyclerViewAdapter.getItemCount() is less than zero.");
+            appAPI.getMainController().fetchPost();
+        } else {
+            Log.d(TAG,"recyclerViewAdapter from memory.");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        appAPI.getMainController().removeMainControllerListener(this);
+    }
+
+    @Override
+    public void onRedditPostListFetchSuccess(List<RedditPost> redditPostList) {
+        Log.d(TAG, "onRedditPostListFetchSuccess redditPostList.size(): " + redditPostList.size());
+        progressBar.setVisibility(View.GONE);
+        errorMessageTextView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.VISIBLE);
+        recyclerViewAdapter.addAll(redditPostList);
+    }
+
+    @Override
+    public void onRedditPostListFetchFailure() {
+        Log.d(TAG, "onRedditPostListFetchFailure()");
+        progressBar.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.GONE);
+        if (recyclerViewAdapter.getItemCount() <= 0) {
+            errorMessageTextView.setVisibility(View.VISIBLE);
+        } else {
+            errorMessageTextView.setVisibility(View.GONE);
+        }
     }
 }
