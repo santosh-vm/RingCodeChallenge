0cef29228288411b03c15a02d750dd1ae2b9fbad
Santosh V. Madhyastha <santosh@gogii.net>
2017-08-25 13:05:50 -0700

    Commenting this line for now.

--- app/src/main/java/android/santosh/com/ringcodechallenge/AppApplication.java
+++ app/src/main/java/android/santosh/com/ringcodechallenge/AppApplication.java
@@ -3,12 +3,14 @@ package android.santosh.com.ringcodechallenge;
 import android.app.Application;
 import android.os.Handler;
 import android.santosh.com.ringcodechallenge.controller.MainController;
+import android.util.Log;
 
 /**
  * Created by Santosh on 8/24/17.
  */
 
 public class AppApplication extends Application {
+    private static String TAG = AppApplication.class.getSimpleName();
     private AppAPI appAPI;
 
     @Override
@@ -21,7 +23,8 @@ public class AppApplication extends Application {
     @Override
     public void onTrimMemory(int level) {
         super.onTrimMemory(level);
-        appAPI.getMainController().resetData();
+        //Log.d(TAG,"onTrimMemory");
+        //appAPI.getMainController().resetData();
     }
 
     public AppAPI getAppAPI() {
