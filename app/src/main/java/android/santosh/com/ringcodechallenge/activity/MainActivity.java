0f7461fe1fe5d2d475b42a2d139bce0f640af4cb
Santosh V. Madhyastha <santosh@gogii.net>
2017-08-24 22:53:14 -0700

    Adding layout files for the recyclerview and updating layout for MainActivity

--- app/src/main/java/android/santosh/com/ringcodechallenge/activity/MainActivity.java
+++ app/src/main/java/android/santosh/com/ringcodechallenge/activity/MainActivity.java
@@ -2,13 +2,44 @@ package android.santosh.com.ringcodechallenge.activity;
 
 import android.santosh.com.ringcodechallenge.R;
 import android.os.Bundle;
+import android.santosh.com.ringcodechallenge.adapter.RecyclerViewAdapter;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends BaseActivity {
     private static String TAG = MainActivity.class.getSimpleName();
+
+    private ProgressBar progressBar;
+    private TextView errorMessageTextView;
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindUIElements();
+        appAPI.getMainController().fetchPost();
+    }
+
+    private void bindUIElements() {
+        //Progress Bar
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+
+        //TextView
+        errorMessageTextView = (TextView) findViewById(R.id.error_message_text_view);
+        errorMessageTextView.setVisibility(View.GONE);
+
+        //RecyclerView
+        recyclerViewAdapter = new RecyclerViewAdapter(this);
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        LinearLayoutManager recyclerViewLinearLayoutManger = new LinearLayoutManager(this);
+        recyclerViewLinearLayoutManger.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(recyclerViewLinearLayoutManger);
+        recyclerView.setAdapter(recyclerViewAdapter);
+        recyclerView.setVisibility(View.GONE);
     }
 }
---
+++ app/src/main/java/android/santosh/com/ringcodechallenge/adapter/RecyclerViewAdapter.java
@@ -0,0 +1,71 @@
+package android.santosh.com.ringcodechallenge.adapter;
+
+import android.content.Context;
+import android.santosh.com.ringcodechallenge.R;
+import android.santosh.com.ringcodechallenge.RedditPostData;
+import android.santosh.com.ringcodechallenge.model.RedditPost;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Santosh on 8/24/17.
+ */
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static String TAG = RecyclerViewAdapter.class.getSimpleName();
+    private Context context;
+    private List<RedditPost> redditPostList = new ArrayList<>();
+
+    public RecyclerViewAdapter(Context context) {
+        this.context = context;
+    }
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_list_item, parent, false);
+        return new RecyclerViewItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+        if (viewHolder instanceof RecyclerViewItemViewHolder) {
+            RecyclerViewItemViewHolder recyclerViewItemViewHolder = (RecyclerViewItemViewHolder) viewHolder;
+            RedditPostData redditPostData = redditPostList.get(position).getRedditPostData();
+            recyclerViewItemViewHolder.titleTextView.setText(redditPostData.getTitle());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return redditPostList.size();
+    }
+
+    public class RecyclerViewItemViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTextView;
+
+        public RecyclerViewItemViewHolder(View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.title_text_view);
+
+        }
+    }
+
+
+    public void add(RedditPost redditPost) {
+        redditPostList.add(redditPost);
+        notifyItemInserted(redditPostList.size() - 1);
+    }
+
+    public void addAll(List<RedditPost> redditPostList) {
+        for (RedditPost redditPost : redditPostList) {
+            add(redditPost);
+        }
+    }
+}
